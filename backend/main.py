980348182a134e0ea8c5141c16f3c14e950d6069
Peeyush Maurya <peeyushmaurya.dev@gmail.com>
2025-09-18 23:08:14 +0530

    update: fixed app import issue

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
-import uvicorn
 import os
+import uvicorn
 from app import app
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", "8000"))
-    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run(app, host="0.0.0.0", port=port, reload=True)
