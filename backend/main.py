2d0df21646ddc7496bdb3905603f5dcc38907663
Peeyush Maurya <peeyushmaurya.dev@gmail.com>
2025-09-21 10:37:06 +0530

    update: backend sync

--- backend/main.py
+++ backend/main.py
@@ -3,5 +3,5 @@ import uvicorn
 from app import app
 
 if __name__ == "__main__":
-    port = int(os.getenv("PORT", "8000"))
-    uvicorn.run(app, host="0.0.0.0", port=port, reload=True)
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=True)
